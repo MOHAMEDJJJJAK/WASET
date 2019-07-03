685a5a3fb00bc2e9fe6f6dcf9824e0f59ce7c5f6
MOHAMEDJJJJAK <52308633+MOHAMEDJJJJAK@users.noreply.github.com>
2019-07-03 19:56:00 +0200

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,15 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`وسيط مضمون نايم`,"http://twitch.tv/وسيط مضمون نايم")
+client.user.setStatus("dnd")
+});
+
+client.login('NTk1NzIwODQ0NDIwOTcyNTYz.XRvNug.WapCJKQEaTOphoL8ObtYe30Az6k');// لا تغير فيها شيء
